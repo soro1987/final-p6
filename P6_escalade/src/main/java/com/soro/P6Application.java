d6d9393ee5dd3dd06e129864d8dc42b00545bced
soro2305 <bcsoroamet@gmail.com>
2020-11-15 11:31:52 +0100

    conversion jar en war

--- P6_escalade/src/main/java/com/soro/P6Application.java
+++ P6_escalade/src/main/java/com/soro/P6Application.java
@@ -2,7 +2,9 @@ package com.soro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import com.soro.storage.StorageProperties;
 
@@ -10,7 +12,12 @@ import com.soro.storage.StorageProperties;
 
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
-public class P6Application {
+public class P6Application extends SpringBootServletInitializer {
+
+	@Override
+	 protected SpringApplicationBuilder configure(SpringApplicationBuilder app) {
+	         return app.sources(P6Application.class);
+	     }
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(P6Application.class, args);
